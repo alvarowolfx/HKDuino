4bb1bbb080f351e41f50bb093b59a6078d39cc47
Alvaro Viebrantz <alvarowolfx@gmail.com>
2016-12-30 17:23:55 -0300

    [feat] Update to MySensors 2.1

--- Relay/src/main.cpp
+++ Relay/src/main.cpp
@@ -1,82 +1,100 @@
 
-#include <MySigningNone.h>
-#include <MyTransportNRF24.h>
-#include <MyHwATMega328.h>
-#include <MySensor.h>
+// NRFRF24L01 radio driver (set low transmit power by default)
+// #define MY_DEBUG
+#define MY_RADIO_NRF24
+#define MY_NODE_ID 3
+#include <MySensors.h>
 
 #define RELAY_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
 #define NUMBER_OF_RELAYS 3 // Total number of attached relays
 #define RELAY_ON 0  // GPIO value to write to turn on attached relay
 #define RELAY_OFF 1 // GPIO value to write to turn off attached relay
 
-// NRFRF24L01 radio driver (set low transmit power by default)
-MyTransportNRF24 radio(RF24_CE_PIN, RF24_CS_PIN, RF24_PA_LEVEL_GW);
-// Select AtMega328 hardware profile
-MyHwATMega328 hw;
-// Construct MySensors library
-MySensor gw(radio, hw);
-
 MyMessage lightMsgs[NUMBER_OF_RELAYS] = {
-        MyMessage(RELAY_1, V_LIGHT),
-        MyMessage(RELAY_1+1, V_LIGHT),
-        MyMessage(RELAY_1+2, V_LIGHT)
+        MyMessage(RELAY_1, V_STATUS),
+        MyMessage(RELAY_1+1, V_STATUS),
+        MyMessage(RELAY_1+2, V_STATUS)
 };
+
+bool initialValueSent = false;
 
 MyMessage getLightMessage(int sensor){
     return lightMsgs[sensor-RELAY_1];
 }
 
-void incomingMessage(const MyMessage &message);
-
 void setup() {
-    // Initialize library and add callback for incoming messages
-    gw.begin(incomingMessage, AUTO, true);
-    // Send the sketch version information to the gateway and Controller
-    gw.sendSketchInfo("Sala Jantar", "1.0");
+  // Fetch relay status
+  for (int i=0, pin=RELAY_1; i<NUMBER_OF_RELAYS;i++, pin++) {
+      // Then set relay pins in output mode
+      Serial.print("Setting sensor :");
+      Serial.println(pin);
+      pinMode(pin, OUTPUT);
+  }
+}
 
-    // Fetch relay status
-    for (int i=0, pin=RELAY_1; i<NUMBER_OF_RELAYS;i++, pin++) {
-        // Register all sensors to gw (they will be created as child devices)
-        Serial.print("Presenting sensor :");
-        Serial.println(pin);
-        gw.present(pin, S_LIGHT);
-        // Then set relay pins in output mode
-        pinMode(pin, OUTPUT);
-        // Set relay to last known state (using eeprom storage)
-        digitalWrite(pin, gw.loadState(pin)?RELAY_ON:RELAY_OFF);
+void presentation(){
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("Sala-Jantar", "1.1");
 
-        Serial.print("Sending initial value:");
-        Serial.println(pin);
-        gw.send(getLightMessage(pin).set(gw.loadState(pin)));
-
-    }
+  // Fetch relay status
+  for (int i=0, pin=RELAY_1; i<NUMBER_OF_RELAYS;i++, pin++) {
+      // Register all sensors to gw (they will be created as child devices)
+      Serial.print("Presenting sensor :");
+      Serial.println(pin);
+      present(pin, S_BINARY, "Interruptor");
+      wait(50);
+  }
 }
 
 
 void loop() {
-    // Alway process incoming messages whenever possible
-    gw.process();
+  if (!initialValueSent) {
+    Serial.println("Requesting initial value");
+    for (int i=0, pin=RELAY_1; i<NUMBER_OF_RELAYS;i++, pin++) {
+        // Then set relay pins in output mode
+        Serial.print("Request initial sensor data :");
+        Serial.println(pin);
+
+        int value = loadState(pin);
+        send(getLightMessage(pin).set(value == RELAY_OFF ? false : true));
+        wait(50);
+        request(pin, V_STATUS);
+        wait(50);
+    }
+    wait(2000);
+  }
+  sendHeartbeat();
 }
 
-void incomingMessage(const MyMessage &message) {
+void receive(const MyMessage &message) {
+
+    if(message.isAck()){
+      Serial.println("This is an ack from gateway");
+    }
     // We only expect one type of message from controller. But we better check anyway.
-    if (message.type==V_LIGHT) {
+    if (message.type == V_STATUS) {
+
+        if (!initialValueSent) {
+          Serial.println("Receiving initial value from controller");
+          initialValueSent = true;
+        }
+
         Serial.print("Incoming change for sensor:");
         Serial.print(message.sensor);
         Serial.print(", With value: ");
         Serial.println(message.getBool());
 
+        int value = message.getBool() ? RELAY_ON: RELAY_OFF;
         // Change relay state
-        digitalWrite(message.sensor, message.getBool()?RELAY_ON:RELAY_OFF);
+        digitalWrite(message.sensor, value);
         // Store state in eeprom
-        gw.saveState(message.sensor, message.getBool());
+        saveState(message.sensor, value);
 
         // Write some debug info
         Serial.print(", New status: ");
         Serial.println(message.getBool());
 
         //ACK
-        gw.send(getLightMessage(message.sensor).set(message.getBool()));
+        send(getLightMessage(message.sensor).set(message.getBool()));
     }
 }
-
